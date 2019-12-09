08b922269f8d24b268f6e0a29d20acd544cb45ff
Elixry Labs <elixryco@gmail.com>
2019-12-08 23:55:55 -0500

    ding

---
+++ js/apps.20191208235437.js
@@ -0,0 +1,51 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
+$.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof attr !== typeof undefined && attr !== false; };
+
+function em1(){var c="fmjyszdpAhnbjm/dpn";var addr="mailto:";for(var i=0;i<c.length;i++)addr+=String.fromCharCode(c.charCodeAt(i)-1);window.location.href=addr;}
+
+$(document).ready(function() {
+if(! document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1")) {
+$('.js-40').attr('src', 'images/twitter-150.png');
+
+}
+(function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(){$(this).click(function(){var t=this.hash.length>1?$('[name="'+this.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
+var wl = new woolite();
+wl.init();
+wl.addAnimation($('.js-5')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-6')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-7')[0], "1.00s", "0.50s", 1, 100);
+wl.addAnimation($('.js-8')[0], "1.00s", "0.90s", 1, 100);
+wl.addAnimation($('.js-9')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-10')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-11')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-12')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-13')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-14')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-15')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-16')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-17')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-18')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-19')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-20')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-21')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-22')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-23')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-24')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-25')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-26')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-27')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-28')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-29')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-30')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-31')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-32')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-33')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-34')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-35')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-36')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-37')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-38')[0], "1.00s", "1.60s", 1, 100);
+wl.addAnimation($('.js-39')[0], "1.00s", "1.60s", 1, 100);
+wl.start();
+
+});
